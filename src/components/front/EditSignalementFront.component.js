ef7f2d92c9e825853d3bc2e0d3a6da1f5647bee6
hasinavald <hasinavaldano00@gmail.com>
2022-02-22 02:06:17 +0300

    commit 4

--- src/components/front/EditSignalementFront.component.js
+++ src/components/front/EditSignalementFront.component.js
@@ -10,81 +10,81 @@ import { MapContainer, TileLayer, Marker, Popup, SVGOverlay } from 'react-leafle
 
 
 const containerStyle = {
-  width: '1110px',
-  height: '400px'
+    width: '1110px',
+    height: '400px'
 };
 
 
 function EditSignalementFront(props) {
 
-      const [id, setId] = useState(0);
+    const [id, setId] = useState(0);
 
-      const [typeSignal, setTypeSignal] = useState('');
-      const [descriptionSignal, setDescriptionSignal] = useState('');
-      const [dateSignal, setDateSignal] = useState('');
-      const [status, setStatusSignal] = useState('');
-      const [image, setImage] = useState('');
-      const [long, setLong] = useState(0);
-      const [lat, setLat] = useState(0);
+    const [typeSignal, setTypeSignal] = useState('');
+    const [descriptionSignal, setDescriptionSignal] = useState('');
+    const [dateSignal, setDateSignal] = useState('');
+    const [status, setStatusSignal] = useState('');
+    const [image, setImage] = useState('');
+    const [long, setLong] = useState(0);
+    const [lat, setLat] = useState(0);
 
 
-      const [sen, setSen] = useState(2);
-      
-      let history = useHistory();
-      const token = props.isAuth;      
-      const isLogged = props.isAuth;
-      
-      useEffect(() => {
+    const [sen, setSen] = useState(2);
+
+    let history = useHistory();
+    const token = props.isAuth;
+    const isLogged = props.isAuth;
+
+    useEffect(() => {
         if(isLogged == false){
-          history.push('login')
-          sessionStorage.removeItem('jwtToken');
+            history.push('login')
+            sessionStorage.removeItem('jwtToken');
         }
 
         async function getDatas(){
 
-          const dataUser = props.signal;
-          if(dataUser){
-            setDescriptionSignal(dataUser.description);
-            setDateSignal(dataUser.date);
-            setStatusSignal(dataUser.status);
-            setImage(dataUser.image);
-  
-            setLong(parseFloat(dataUser.longitude));
-            setLat(parseFloat(dataUser.latitude));
-            setId(parseInt(dataUser.id));
-            setTypeSignal(dataUser.typeSignal[0].type)
-          }
-          
+            const dataUser = props.signal;
+            if(dataUser){
+                setDescriptionSignal(dataUser.description);
+                setDateSignal(dataUser.date);
+                setStatusSignal(dataUser.status);
+                setImage(dataUser.image);
+
+                setLong(parseFloat(dataUser.longitude));
+                setLat(parseFloat(dataUser.latitude));
+                setId(parseInt(dataUser.id));
+                setTypeSignal(dataUser.typeSignal[0].type)
+            }
+
         }
-        
+
         getDatas();
-        
-      },[]);
-      if(lat !=0){
+
+    },[]);
+    if(lat !=0){
         var mape = (
-                  <MapContainer center={[lat,long]} style={{width:"100%",height:"300px"}} zoom={5} scrollWheelZoom={true}>
-                  <TileLayer
+            <MapContainer center={[lat,long]} style={{width:"100%",height:"300px"}} zoom={5} scrollWheelZoom={true}>
+                <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  />
-                  <Marker position={[lat,long]}>
-                  </Marker>
-                </MapContainer>
+                />
+                <Marker position={[lat,long]}>
+                </Marker>
+            </MapContainer>
         );
-        
-      }else{
+
+    }else{
         var mape = "";
-      }
-      function Back(){
+    }
+    function Back(){
         history.push("dashboard-front");
-      }
-      
-      async function Valider(){
-       
+    }
+
+    async function Valider(){
+
         const requestOptions = {
-          method: 'PUT',
-          headers: { 'Content-Type': 'application/json', 'Authorization' : "Bearer "+token},
-          body: status
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json', 'Authorization' : "Bearer "+token},
+            body: status
         };
 
         const fetchData = async () => {
@@ -93,96 +93,95 @@ function EditSignalementFront(props) {
         }
 
         if(status == "Terminé"){
-          const requestOptions2 = {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json', 'Authorization' : "Bearer "+token},
-            body: status
-          };
-  
-          const fetchData2= async () => {
-              const result = await fetch(configData.SERVER_URL+"signal/clen/seenno/"+id, requestOptions2);
-              const data = await result.json();
-          }
+            const requestOptions2 = {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json', 'Authorization' : "Bearer "+token},
+            };
+
+            const fetchData2= async () => {
+                const result = await fetch(configData.SERVER_URL+"signal/clen/seenno?id="+id, requestOptions2);
+                const data = await result.json();
+            }
         }
 
-      
+
         await fetchData();
         history.push("dashboard-front");
 
-      }
+    }
 
-      function changeStatusSignals(e){
+    function changeStatusSignals(e){
         setStatusSignal(e.target.value);
-      }
+    }
 
 
-        return (
-            <div className="container container_page">
-              <div>
+    return (
+        <div className="container container_page">
+            <div>
                 <MenusFront />
-              </div>
+            </div>
 
-              <h3 className="title_signal">Modifier le Signalement</h3>
-              <button onClick={Back.bind(this)} className="btn btn-info mb-4">Retour</button>
-              <div className="">
+            <h3 className="title_signal">Modifier le Signalement</h3>
+            <button onClick={Back.bind(this)} className="btn btn-info mb-4">Retour</button>
+            <div className="">
                 <div className="">
                     {mape}
                 </div>
-              </div>
-
-              <div className="gridRow">
-                <div className="img_container_signal">
-                  <img width="100%" src={"data:image/jpeg;base64,"+image} />
-                </div>
-              <div className="blackBoardCOntainer">
-
-                <div className="blackboard">
-                  <div className="form">
-                    <div className="formGrp">
-                        <label className="label_form" >Description</label>
-                        <input disabled="disabled" className="formInptextarea" value={descriptionSignal} />
-                    </div>
-
-                    <div className="formGrp">
-                        <label  className="label_form" >Type</label>
-                        <input disabled="disabled" value={typeSignal} type="text" className="formInpt" />
-                    </div>
-
-                    <div className="formGrp">
-                      <label className="label_form" >Status</label>
-                        <select onChange={changeStatusSignals.bind(this)} className="formInpt">
-                            <option selected={(status == 'Nouveau') ? "selected" : "" } >Nouveau</option>
-                            <option selected={(status == 'En cours de traitement') ? "selected" : "" }>En cours de traitement</option>
-                            <option selected={(status == 'Terminé') ? "selected" : "" }>Terminé</option>
-                        </select>
-                    </div>
-
-                    <div className="formGrp">
-                        <button onClick={Valider.bind(this)} className="bouton_update"><span className="text">Valider</span></button>
-                    </div>
-
-                  </div>
-                </div>
-
-              </div>
-
-              </div>
-              
             </div>
-            );
-        }
+
+            <div className="gridRow">
+                <div className="img_container_signal">
+                    <img width="100%" src={"data:image/jpeg;base64,"+image} />
+                </div>
+                <div className="blackBoardCOntainer">
+
+                    <div className="blackboard">
+                        <div className="form">
+                            <div className="formGrp">
+                                <label className="label_form" >Description</label>
+                                <input disabled="disabled" className="formInptextarea" value={descriptionSignal} />
+                            </div>
+
+                            <div className="formGrp">
+                                <label  className="label_form" >Type</label>
+                                <input disabled="disabled" value={typeSignal} type="text" className="formInpt" />
+                            </div>
+
+                            <div className="formGrp">
+                                <label className="label_form" >Status</label>
+                                <select onChange={changeStatusSignals.bind(this)} className="formInpt">
+                                    <option selected={(status == 'Nouveau') ? "selected" : "" } >Nouveau</option>
+                                    <option selected={(status == 'En cours de traitement') ? "selected" : "" }>En cours de traitement</option>
+                                    <option selected={(status == 'Terminé') ? "selected" : "" }>Terminé</option>
+                                </select>
+                            </div>
+
+                            <div className="formGrp">
+                                <button onClick={Valider.bind(this)} className="bouton_update"><span className="text">Valider</span></button>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    );
+}
 
 const states = (state) => {
-  return {
-    isAuth : state.isAuth,
-    signal : state.signal
-  }
+    return {
+        isAuth : state.isAuth,
+        signal : state.signal
+    }
 }
 
 const setAuth = (dispatch) => {
-  return {
-    setAuth : (val) => {dispatch({type : 'SET_SESSIONS',value : val})}
-  } 
+    return {
+        setAuth : (val) => {dispatch({type : 'SET_SESSIONS',value : val})}
+    }
 }
 
 export default connect(states,setAuth)(EditSignalementFront);
